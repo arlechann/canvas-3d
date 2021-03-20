06c8c0402062f389283def977234dd768a844ded
arlechann <dragnov3728@gmail.com>
2021-03-21 04:09:28 +0900

    Fix assertion message

--- test/lib/assert.ts
+++ test/lib/assert.ts
@@ -6,6 +6,6 @@ export namespace Assert {
 
 	export const equalBy = <T>(actual: T, expect: T, comp: (act: T, exp: T) => boolean): void => {
 		console.log('.');
-		console.assert(comp(actual, expect), `${expect} expected but actually ${actual}`);
+		console.assert(comp(actual, expect), `${JSON.stringify(expect)} expected but actually ${JSON.stringify(actual)}`);
 	};
 }
