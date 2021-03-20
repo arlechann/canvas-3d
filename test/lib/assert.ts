0cc68cc0714898e0acb2c1153bed276493a674c8
arlechann <dragnov3728@gmail.com>
2021-03-21 03:58:25 +0900

    Add assertEqualBy

--- test/lib/assert.ts
+++ test/lib/assert.ts
@@ -2,3 +2,8 @@ export const assertEqual = <T>(actual: T, expect: T): void => {
 	console.log('.');
 	console.assert(actual === expect, `${expect} expected but actually ${actual}`);
 };
+
+export const assertEqualBy = <T>(actual: T, expect: T, comp: (act: T, exp: T) => boolean): void => {
+	console.log('.');
+	console.assert(comp(actual, expect), `${expect} expected but actually ${actual}`);
+};
