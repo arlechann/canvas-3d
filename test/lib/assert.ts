93c777de5c32059ebce5dbf15180f2d102842335
arlechann <dragnov3728@gmail.com>
2021-03-21 03:59:38 +0900

    Use namespace

--- test/lib/assert.ts
+++ test/lib/assert.ts
@@ -1,9 +1,11 @@
-export const assertEqual = <T>(actual: T, expect: T): void => {
-	console.log('.');
-	console.assert(actual === expect, `${expect} expected but actually ${actual}`);
-};
+export namespace Assert {
+	export const equal = <T>(actual: T, expect: T): void => {
+		console.log('.');
+		console.assert(actual === expect, `${expect} expected but actually ${actual}`);
+	};
 
-export const assertEqualBy = <T>(actual: T, expect: T, comp: (act: T, exp: T) => boolean): void => {
-	console.log('.');
-	console.assert(comp(actual, expect), `${expect} expected but actually ${actual}`);
-};
+	export const equalBy = <T>(actual: T, expect: T, comp: (act: T, exp: T) => boolean): void => {
+		console.log('.');
+		console.assert(comp(actual, expect), `${expect} expected but actually ${actual}`);
+	};
+}
