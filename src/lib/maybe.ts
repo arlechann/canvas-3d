f9befa0b685ae77d8cb500f045f0b3980d96f34e
arlechann <dragnov3728@gmail.com>
2021-03-21 03:42:19 +0900

    Add Maybe module

---
+++ src/lib/maybe.ts
@@ -0,0 +1,56 @@
+export namespace Maybe {
+	export function wrap<T>(a: T): Maybe<NonNullable<T>> {
+		if (a == null) { new Nothing<NonNullable<T>>(); }
+		return new Just<NonNullable<T>>(a as NonNullable<T>);
+	};
+
+	export type Maybe<T> = Just<T> | Nothing<T>;
+
+	export class Just<T> {
+		constructor(private value: T) { }
+
+		map<U>(f: (a: T) => U): Maybe<NonNullable<U>> {
+			return wrap(f(this.value));
+		}
+
+		forEach(f: (a: T) => void): void {
+			f(this.value);
+		}
+
+		flatMap<U>(f: (a: T) => Maybe<U>): Maybe<U> {
+			return f(this.value);
+		}
+
+		then<U>(f: () => Maybe<U>): Maybe<U> {
+			return f();
+		}
+
+		isJust(): this is Just<T> { return true; }
+		isNothing(): this is Nothing<T> { return false; }
+		unsafeGet(): T { return this.value; }
+		throwableGet(msg?: string): T { return this.value; }
+	}
+
+	export class Nothing<T> {
+		constructor(value?: T) { }
+
+		map<U>(f: (a: T) => U): Nothing<NonNullable<U>> {
+			return new Nothing();
+		}
+
+		forEach(f: (a: T) => void): void { }
+
+		flatMap<U>(f: (a: T) => Maybe<U>): Maybe<U> {
+			return new Nothing();
+		}
+
+		then<U>(f: () => Maybe<U>): Maybe<U> {
+			return new Nothing();
+		}
+
+		isJust(): this is Just<T> { return false; }
+		isNothing(): this is Nothing<T> { return true; }
+		unsafeGet(): null { return null; }
+		throwableGet(msg?: string): never { throw new Error(msg ?? 'GetValueFromNothingObjectError'); }
+	}
+};
